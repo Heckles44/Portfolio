e608e800d8d4e1fd0041e1d33b619181bcbe76d2
Dave <davidballock@gmail.com>
2020-10-23 13:51:43 +1100

    address mobile responsiveness

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class TypeWriter {
-  constructor(txtElement, words, wait = 3000) {
+  constructor(txtElement, words, wait = 2000) {
     this.txtElement = txtElement;
     this.words = words;
     this.txt = "";
@@ -27,7 +27,7 @@ class TypeWriter {
     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
     //Initial Type Speed
-    let typeSpeed = 300;
+    let typeSpeed = 100;
     if (this.isDeleting) {
       typeSpeed /= 2;
     }
@@ -44,7 +44,7 @@ class TypeWriter {
       // move to the next word
       this.wordIndex++;
       // Pause before start typing
-      typeSpeed = 500;
+      typeSpeed = 200;
     }
 
     setTimeout(() => this.type(), typeSpeed);
